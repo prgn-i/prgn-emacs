ac2c6bc697568274c7f67fba6773b914f50ce67a
prgn-i <prgn-i@proton.me>
2022-11-13 10:50:24 +0200

    Added early initialization configuration file

---
+++ early-init.el
@@ -0,0 +1,6 @@
+;; early-init.el
+
+;; Add "lib/" directory to list of directories searched during initial load
+(add-to-list 'load-path (concat user-emacs-directory "lib"))
+
+;; early-init.el ends here
