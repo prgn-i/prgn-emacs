679ad61f385a305a5839f2f580cb0a98278c9a8a
prgn-i <prgn-i@proton.me>
2022-11-17 08:56:10 +0200

    Updated package management to use straight

--- plugin/bootstrap.el
+++ plugin/bootstrap.el
@@ -1,23 +1,22 @@
 ;; bootstrap.el
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org"   . "https://orgmode.org/elpa/")
-			 ("elpa"  . "https://elpa.gnu.org/packages/")))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; Ensure package archive is bootstrapped
+;; Integrate `straight' into `use-package'
+(straight-use-package 'use-package)
 
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-;; Bootstrap `use-package'
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(require 'use-package)
-
-;; `use-package' options
-
-(setq use-package-always-ensure t)
+;; Enable using `straight' in `use-package' by default
+(setq straight-use-package-by-default t)
 
 ;; bootstrap.el ends here
